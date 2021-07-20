50ada803e5b6963aa7c386e4182225e13628293f
Xavier Blondel <blondel.xavier+uf@gmail.com>
2021-07-20 07:40:36 +0200

    add model

---
+++ src/models/Thumbnails.ts
@@ -0,0 +1,111 @@
+import path from 'path';
+import sqlite3 from "sqlite3";
+
+export class Thumbnails {
+    private dbFullpath : string
+
+    constructor (dbPath = process.env.DB_PATH) {
+        if (!dbPath) {
+            throw new Error("No path provided and DB_PATH not set");
+        }
+        const rootPath = `${path.dirname(__filename)}/../..`;
+        const targetPath = path.normalize(`${rootPath}/${dbPath}`);
+        this.dbFullpath = targetPath;
+    }
+
+    setup() {
+        return this.run(
+            `
+            CREATE TABLE IF NOT EXISTS pdf_thumbnails (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                url TEXT NOT NULL,
+                thumbnail TEXT NOT NULL
+            );
+            `
+        );
+    }
+
+    async insert(url: string, thumbnail: string) : Promise<void> {
+        return this.run(
+            `
+            INSERT INTO pdf_thumbnails (url, thumbnail)
+            VALUES(?, ?)
+            `,
+            [url, thumbnail]
+        )
+    }
+
+    async fetch(from: number = 0, size: number = 0) : Promise<any[]> {
+        let rows: any[] = [];
+        const row = await this.all(
+        `
+            SELECT id, url, thumbnail FROM pdf_thumbnails
+        `
+        );
+        return rows;
+    }
+
+    async exists(url: string) : Promise<boolean> {
+        let exists = false;
+        const rows = await this.get(
+            `
+            SELECT 1
+            FROM pdf_thumbnails
+            WHERE url = ?
+            `,
+            [url],
+        );
+        return rows.length > 0;
+    }
+
+    private db() : sqlite3.Database {
+        return new sqlite3.Database(this.dbFullpath, (err: Error | null) => {
+            if (err) {
+                throw err;
+            }
+        });
+
+    }
+    private async get(sql: string, params: any[] = []) : Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            const db = this.db();
+            db.get(sql, params, (err, data) => {
+                db.close();
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    private async all(sql: string, params: any[] = []) : Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            const db = this.db();
+            db.all(sql, params, (err, data) => {
+                db.close();
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    private async run(sql: string, params: any[] = []) : Promise<void> {
+        return new Promise((resolve, reject) => {
+            const db = this.db();
+            db.run(sql, params, (err) => {
+                db.close();
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
+
